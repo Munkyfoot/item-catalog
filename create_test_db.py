5f7781eb096c921fa8eeaed9536a6ebcc5ed9611
Munkyfoot <17400831+Munkyfoot@users.noreply.github.com>
2018-11-07 14:31:42 -0800

    Setup catalog.html and a python script to create a test database for testing

---
+++ create_test_db.py
@@ -0,0 +1,28 @@
+from db import DB_NAME, Base, User, Category, Item
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, asc, desc
+
+engine = create_engine('sqlite:///{}.db'.format(DB_NAME))
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+
+session = DBSession()
+
+user = User(name='Username', email='test@test.com', picture='/')
+
+categories = ['Music', 'Games', 'Videos']
+
+items = ['Item_1', 'Item_2', 'Item_3']
+
+cat_id = 0
+for c in categories:
+    category = Category(name=c)
+    session.add(category)
+    session.commit()
+
+    for i in items:
+        item = Item(name=i,description='Item Description',category_id=cat_id,user_id=0)
+        session.add(item)
+        session.commit()
+
