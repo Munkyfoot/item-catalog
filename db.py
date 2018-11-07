0b53e01f5666fc07c1f43fb240573a8c6c9d2c66
Munkyfoot <17400831+Munkyfoot@users.noreply.github.com>
2018-11-07 12:41:08 -0800

    Add depndencies and setup tables for database

---
+++ db.py
@@ -0,0 +1,61 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+DB_NAME = 'catalog'
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(150), nullable=False)
+    email = Column(String(150), nullable=False)
+    picture = Column(String(150), nullable=False)
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
+
+class Item(Base):
+    __tablename__ = 'item'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    description = Column(String(300), nullable=False)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'category_id': self.category_id,
+            'user_id': self.user_id
+        }
+
+
+engine = create_engine('sqlite:///{}.db'.format(DB_NAME))
+
+
+Base.metadata.create_all(engine)
