d79c66f9ef6b29617a49db4006f18de8ed10b717
Munkyfoot <17400831+Munkyfoot@users.noreply.github.com>
2018-11-13 13:00:08 -0800

    Remove unnecessary javascript

--- static/scripts/item.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    var item_image_display = $('#item_image_display');
-
-    if(item_image_display != null){
-        item_image_display.width(item_image_display.parent().width());
-    }
-});
